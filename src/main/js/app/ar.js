3b33a3f75b5fb69b333ef5d1b1ca826f3ed82e5e
Joe Mirizio <joe@joemirizio.com>
2016-04-10 12:48:00 -0400

    Implement AR pixelation grid

--- src/main/js/app/ar.js
+++ src/main/js/app/ar.js
@@ -152,7 +152,7 @@ function debugMarker(ctx, marker, frameTime) {
 }
 
 function processMarker(ctx, marker, frameTime) {
-    var verticies = marker.vertex;
+    var quadVerts = marker.vertex;
     var center = marker.pos;
     var padding = 1.5;
 
@@ -160,59 +160,24 @@ function processMarker(ctx, marker, frameTime) {
     ctx.translate(center[0] * (1 - padding), center[1] * (1 - padding));
     ctx.scale(padding, padding);
 
-    ctx.shadowColor = 'rgba(52, 204, 255, 1)';
-    ctx.shadowBlur = 10;
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = 'rgba(52, 204, 255, 1)';
-    ctx.fillStyle = 'rgba(52, 204, 255, 0.5)';
-    ctx.beginPath();
-    ctx.moveTo(verticies[0][0], verticies[0][1]);
-    ctx.lineTo(verticies[1][0], verticies[1][1]);
-    ctx.lineTo(verticies[2][0], verticies[2][1]);
-    ctx.lineTo(verticies[3][0], verticies[3][1]);
-    ctx.lineTo(verticies[0][0], verticies[0][1]);
-    ctx.fill();
-    ctx.stroke();
+    // ctx.shadowColor = 'rgba(52, 204, 255, 1)';
+    // ctx.shadowBlur = 10;
+    // ctx.lineWidth = 2;
+    // ctx.strokeStyle = 'rgba(52, 204, 255, 1)';
+    // ctx.fillStyle = 'rgba(52, 204, 255, 0.5)';
+    // ctx.beginPath();
+    // ctx.moveTo(quadVerts[0][0], quadVerts[0][1]);
+    // ctx.lineTo(quadVerts[1][0], quadVerts[1][1]);
+    // ctx.lineTo(quadVerts[2][0], quadVerts[2][1]);
+    // ctx.lineTo(quadVerts[3][0], quadVerts[3][1]);
+    // ctx.lineTo(quadVerts[0][0], quadVerts[0][1]);
+    // ctx.fill();
+    // ctx.stroke();
+    //ctx.restore();
 
-    // var width = verticies[0][0] - verticies[1][0];
-    // var height = verticies[0][1] - verticies[3][1];
-    //
-    // var slices = 2;
-    //
-    // function lerp(v0, v1, t) {
-    //       return (1 - t) * v0 + t * v1;
-    // }
-    //
-    // ctx.translate(-verticies[0][0], -verticies[0][1]);
-    // for (var y = 0; y < slices; y++) {
-    //     for (var x = 0; x < slices; x++) {
-    //         //var color = Math.floor(Math.random() * 255);
-    //         var colorNum = Math.pow(slices, 2);
-    //         //var color = Math.floor((x / colorNum + y * colorNum) % 255);
-    //         var color = 128;
-    //         ctx.fillStyle = 'rgba(' + color + ', ' + color + ', ' + color + ', 0.5)';
-    //         var xPer = x/slices;
-    //         var xPer2 = (x + 1)/slices;
-    //         var yPer = y/slices;
-    //         var yPer2 = (y + 1)/slices;
-    //         ctx.beginPath();
-    //         //console.log(verticies[0], verticies[1], verticies[2], verticies[3]);
-    //         ctx.moveTo(lerp(verticies[0][0], verticies[1][0], xPer) + lerp(verticies[0][0], verticies[3][0], yPer),
-    //                    lerp(verticies[0][1], verticies[1][1], xPer) + lerp(verticies[0][1], verticies[3][1], yPer));
-    //         ctx.lineTo(lerp(verticies[0][0], verticies[1][0], xPer2) + lerp(verticies[1][0], verticies[2][0], yPer),
-    //                    lerp(verticies[0][1], verticies[1][1], xPer2) + lerp(verticies[1][1], verticies[2][1], yPer));
-    //         ctx.lineTo(lerp(verticies[3][0], verticies[2][0], xPer2) + lerp(verticies[1][0], verticies[2][0], yPer2),
-    //                    lerp(verticies[3][1], verticies[2][1], xPer2) + lerp(verticies[1][1], verticies[2][1], yPer2));
-    //         ctx.lineTo(lerp(verticies[3][0], verticies[2][0], xPer) + lerp(verticies[0][0], verticies[3][0], yPer2),
-    //                    lerp(verticies[3][1], verticies[2][1], xPer) + lerp(verticies[0][1], verticies[3][1], yPer2));
-    //         //ctx.lineTo(lerp(verticies[0][0], verticies[1][0], xPer2), lerp(verticies[0][1], verticies[1][1], yPer2));
-    //         //ctx.lineTo(lerp(verticies[3][0], verticies[2][0], xPer2), lerp(verticies[3][1], verticies[2][1], yPer2));
-    //         //ctx.lineTo(lerp(verticies[3][0], verticies[2][0], xPer), lerp(verticies[3][1], verticies[2][1], yPer));
-    //         //ctx.lineTo(lerp(verticies[0][0], verticies[1][0], xPer), lerp(verticies[0][1], verticies[1][1], yPer));
-    //         ctx.fill();
-    //     }
-    // }
-    // ctx.translate(verticies[0][0], verticies[0][1]);
+    var slices = 5;
+    drawPixelatedGrid(ctx, quadVerts, slices);
+    //ctx.translate(-quadVerts[0][0], -quadVerts[0][1]);
     ctx.restore();
 
     // Update found markers
@@ -220,5 +185,43 @@ function processMarker(ctx, marker, frameTime) {
     if (marker) {
         // TODO Update model with apply
         foundMarkers[marker.id - 1] = { id: marker.id, found: true };
+    }
+}
+
+function lerp(v0, v1, t) {
+      return (1 - t) * v0 + t * v1;
+}
+
+function drawPixelatedGrid(ctx, quadVerts, slices) {
+    var x, y;
+    var points = [];
+    for (y = 0; y < slices + 1; y++) {
+        var yPer = y / slices;
+        var y1 = [lerp(quadVerts[0][0], quadVerts[3][0], yPer), lerp(quadVerts[0][1], quadVerts[3][1], yPer)];
+        var y2 = [lerp(quadVerts[1][0], quadVerts[2][0], yPer), lerp(quadVerts[1][1], quadVerts[2][1], yPer)];
+        points[y] = [];
+        for (x = 0; x < slices + 1; x++) {
+            var xPer = x / slices;
+            var xPoint = [lerp(y1[0], y2[0], xPer), lerp(y1[1], y2[1], xPer)];
+            points[y].push(xPoint);
+        }
+    }
+
+    for (y = 0; y < points.length - 1; y++) {
+        for (x = 0; x < points[y].length - 1; x++) {
+            var color = Math.floor(Math.random() * 255);
+            ctx.fillStyle = 'rgba(' + color + ', ' + color + ', ' + color + ', 1)';
+
+            ctx.beginPath();
+            var p1 = points[x][y];
+            var p2 = points[x + 1][y];
+            var p3 = points[x + 1][y + 1];
+            var p4 = points[x][y + 1];
+            ctx.moveTo(p1[0], p1[1]);
+            ctx.lineTo(p2[0], p2[1]);
+            ctx.lineTo(p3[0], p3[1]);
+            ctx.lineTo(p4[0], p4[1]);
+            ctx.fill();
+        }
     }
 }
